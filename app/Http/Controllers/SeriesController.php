83087a768e966368364599010e7b5fabf6b163b3
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-09 16:05:46 -0300

    Separar view do controller

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -8,19 +8,12 @@ class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        echo $request->url() . PHP_EOL;
-        var_dump($request->query());
         $series = [
             'Breaking Bad',
             'The Office',
             'Westworld',
         ];
-        $html = "<ul>";
-        foreach($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-        $html .= "</ul>";
-    
-        return $html;
+        
+        return view('series.index', compact('series'));
     }
 }
