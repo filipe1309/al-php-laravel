b551fc0573c7c83216495e2c3da8e9aa934de0cd
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-10 16:42:46 -0300

    Tornar autenticao obrigatoria para todas rotas automaticamente

--- app/Http/Middleware/Autenticador.php
+++ app/Http/Middleware/Autenticador.php
@@ -17,7 +17,7 @@ class Autenticador
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!Auth::check()) {
+        if (!$request->is('entrar', 'registrar') && !Auth::check()) {
             return redirect('/entrar');
         }
 
