fcb55db17d452aa61a5d77d13e2af9a63ffa8d4c
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-09-09 21:50:40 -0300

    Refatorar a criacao de series

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Serie;
+use App\Services\CriadorDeSerie;
 use Illuminate\Http\Request;
 use App\Http\Requests\SeriesFormRequest;
 
@@ -22,17 +23,9 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(SeriesFormRequest $request)
-    {
-        $serie = Serie::create(['nome' => $request->nome]);
-        $qtdTemporadas = $request->qtd_temporadas;
-        for ($i=1; $i <= $qtdTemporadas; $i++) { 
-            $temporada = $serie->temporadas()->create(['numero' => $i]);
-
-            for ($j=1; $j <= $request->ep_por_temporada; $j++) { 
-                $temporada->episodios()->create(['numero' => $j]);
-            }
-        }
+    public function store(SeriesFormRequest $request, CriadorDeSerie $criadorDeSerie)
+    {;
+        $serie = $criadorDeSerie->criarSerie($request->nome, $request->qtd_temporadas, $request->ep_por_temporada);
 
         $request->session()->flash('mensagem', "Serie {$serie->id}, temporadas, e episodios criados com sucesso: {$serie->nome}");
 
